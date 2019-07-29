486184e0051bfd2f479020d4f2ec564731a48ebf
krishnachalise418 <krishnachalise418@gmail.com>
2019-07-29 22:19:22 +0545

    FA: database config method; addQuestion simple api

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/questions", handlers.AddQuestions )
+	r.GET("/questions", handlers.AddQuestions)
 	r.Run(":8000") // listen and serve on 0.0.0.0:8080
 }
