5e41f006f3b9e445efca13b7e61161c90622d325
krishnachalise418 <krishnachalise418@gmail.com>
2019-10-21 21:19:40 +0545

    refactored routes

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/zero2her0/driving-license/handlers"
-)
+import "github.com/zero2her0/driving-license/routes"
 
 func main() {
-	r := gin.Default()
-	r.GET("/questions", handlers.AddQuestions)
-	r.Run(":8000") // listen and serve on 0.0.0.0:8080
+	routes.InitRoutes()
 }
